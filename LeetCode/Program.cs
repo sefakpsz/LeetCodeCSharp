6aa2b836f5c6a9f32b3d7aba5645b7ad025cab55
sefa kapısız <sefa.kapisiz@coino.com>
2022-11-23 09:27:17 +0300

    t

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,1 +1,10 @@
 ﻿//third
+var s = Console.ReadLine();
+Console.WriteLine(new Solution().RomanToInt(s));
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        return 0;
+    }
+}
