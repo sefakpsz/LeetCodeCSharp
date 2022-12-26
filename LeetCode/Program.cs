a843675b32f4be8c48f8388ba9ae4f4d588a8139
sefa kapısız <sefa.kapisiz@coino.com>
2022-12-26 15:02:45 +0300

    upd

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,7 +1,74 @@
-﻿public class Solution
+﻿while (true)
 {
-    public int RomanToInt(string s)
+    var roman = Console.ReadLine().ToUpper();
+    Console.WriteLine(Solution.RomanToInt(roman));
+}
+public static class Solution
+{
+    public static int RomanToInt(string s)
     {
-
+        if (!s.Contains('M'))
+        {
+            if (!s.Contains('D'))
+            {
+                if (!s.Contains('C'))
+                {
+                    if (!s.Contains('X'))
+                    {
+                        if (!s.Contains('V'))
+                        {
+                            if(s.Length>3)
+                            {
+                                throw new Exception("Error: Invalid Value Entered!");
+                            }
+                            return s.Length;
+                        }
+                        if (s.Length > 4)
+                        {
+                            throw new Exception("Error: Invalid Value Entered!");
+                        }
+                        else if (s[0].ToString().ToUpper() != "V")
+                        {
+                            return 4;
+                        }
+                        else if(s.Contains('I'))
+                        {
+                            return s.LastIndexOf("I") + 5;
+                        }
+                        else
+                        {
+                            return 5;
+                        }
+                    }
+                    if (s[0] == 'I')
+                    {
+                        if (s.Length > 2)
+                        {
+                            throw new Exception("Error: Invalid Value Entered!");
+                        }
+                        return 9;
+                    }
+                    if (s.Contains('V'))
+                    {
+                        if (s.Length > 5)
+                        {
+                            throw new Exception("Error: Invalid Value Entered!");
+                        }
+                        return 15 + (s.Length - 2);
+                    }
+                    else
+                    {
+                        if (s.Length > 4)
+                        {
+                            throw new Exception("Error: Invalid Value Entered!");
+                        }
+                        return 10 + (s.Length - 1);
+                    }
+                }
+                return 0;
+            }
+            return 0;
+        }
+        return 0;
     }
 }
